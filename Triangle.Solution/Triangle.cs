eefe2209b4365e26905a68f3804e0921b2271300
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-23 10:19:38 -0800

    create  layout for BL
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ Triangle.Solution/Triangle.cs
@@ -0,0 +1,11 @@
+namespace Triangle
+{
+  public class Triangle
+  {
+    public string ValidateTriangle(int side1, int side2, int side3) 
+    {
+      //logic will go here
+      return side1+side2+side3;
+    }
+  }
+}

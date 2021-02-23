0d0475cdd61819a120434ecb56398330b6b98adf
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-23 11:36:30 -0800

    pass test#2
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Triangle.Solution/Triangle/Models/Triangle.cs
+++ Triangle.Solution/Triangle/Models/Triangle.cs
@@ -4,7 +4,10 @@ namespace Triangle
   {
     public string ValidateTriangle(int side1, int side2, int side3) 
     {
-      //logic will go here
+      if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+      {
+        return "invalid";
+      }
       return "valid";
     }
   }

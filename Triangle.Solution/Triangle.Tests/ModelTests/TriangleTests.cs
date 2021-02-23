595d95149c63660366185f16e5916e50591523af
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-23 11:34:15 -0800

    fail test#2
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- Triangle.Solution/Triangle.Tests/ModelTests/TriangleTests.cs
+++ Triangle.Solution/Triangle.Tests/ModelTests/TriangleTests.cs
@@ -12,5 +12,12 @@ namespace Triangle.Tests
             Triangle testTriangle = new Triangle();
             Assert.AreEqual("valid", testTriangle.ValidateTriangle(3,4,5));
         }
+
+        [TestMethod]
+        public void ValidateTriangle_ValidSides_Invalid()
+        {
+            Triangle testTriangle = new Triangle();
+            Assert.AreEqual("invalid", testTriangle.ValidateTriangle(3,-4,0));
+        }
     }
 }
